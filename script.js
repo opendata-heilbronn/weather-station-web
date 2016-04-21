93874efb4a8974b6ba7d57f9f1ea732b6d30ac4f
Lukas Himsel <lukas.himsel@web.de>
2016-04-21 14:18:12 +0200

    moment.js

--- script.js
+++ script.js
@@ -39,7 +39,9 @@ var svg = d3.select("#stats").append("svg")
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-d3.json("http://api.grundid.de/sensor?sensorName=cowo.outside.temperature,cowo.inside1.temperature,cowo.inside2.temperature&size=99999999", function (error, data) {
+
+
+d3.json("http://api.grundid.de/sensor?sensorName=cowo.outside.temperature,cowo.inside1.temperature,cowo.inside2.temperature&size=99999999&from", function (error, data) {
     if (error) throw error;
 
     var sensors = {};
