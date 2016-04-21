26de06af2210a1eb705eeacb0af3444750bab4a5
Lukas Himsel <lukas.himsel@web.de>
2016-04-21 14:23:38 +0200

    statistics

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ var svg = d3.select("#stats").append("svg")
 
 
 
-d3.json("http://api.grundid.de/sensor?sensorName=cowo.outside.temperature,cowo.inside1.temperature,cowo.inside2.temperature&size=99999999&from", function (error, data) {
+d3.json("http://api.grundid.de/sensor?sensorName=cowo.outside.temperature,cowo.inside1.temperature,cowo.inside2.temperature&size=99999999&from=" + moment().subtract(7, 'days').format('x'), function (error, data) {
     if (error) throw error;
 
     var sensors = {};
